38549f4360c001cd4367fe2dece2f4422cae0e58
quoctdinh <43261314+quoctdinh@users.noreply.github.com>
2018-09-17 18:19:27 +1000

    Added checking for file existence.

--- simple_md_httpd_server.py
+++ simple_md_httpd_server.py
@@ -4,28 +4,46 @@ import SocketServer
 import markdown
 
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-	def do_GET(self):
-		file_path = self.path
-		file_ext  = file_path.split( '.' )[-1]
-		file_path = file_path.replace( '/', '' )
+    def do_GET(self):
+        file_path = self.path
+        file_ext  = file_path.split( '.' )[-1]
+        file_path = file_path.replace( '/', '' )
 
-		if file_ext == 'md':
-			self.send_response( 200 )
-			self.send_header('Content-type', 'text/html')
-			self.end_headers()
+        if file_ext == 'md':
+            html = ''
+            try:
+                with open( file_path, 'r' ) as mdfile:
+                    md   = mdfile.read()
+                    extensions = ['extra', 'smarty']
+                    html = markdown.markdown(md, extensions=extensions, output_format='html5')
+            except:
+                pass
 
-			with open( file_path, 'r' ) as mdfile:
-				md = mdfile.read()
-				extensions = ['extra', 'smarty']
-				html = markdown.markdown(md, extensions=extensions, output_format='html5')
-				return self.wfile.write( html )
-			
-		return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+            if html:
+                self.send_response( 200 )
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                return self.wfile.write( html )
+
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+
+class MyTCPServer(SocketServer.TCPServer):
+    def server_bind(self):
+        import socket
+        self.socket.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
+        self.socket.bind(self.server_address)
+
 
 Handler = MyRequestHandler
-server  = SocketServer.TCPServer(('0.0.0.0', 8080), Handler)
+server  = MyTCPServer(( '0.0.0.0', 8080 ), Handler )
+
 
 try:
-	server.serve_forever()
+    server.serve_forever()
 except KeyboardInterrupt:
-	server.server_close()
+    pass
+
+server.server_close()
+server.shutdown()
